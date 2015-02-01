bc4e58618743d3e2695653fc8840122a9a36417f
Maria <maria.sam@gmail.com>
2015-01-31 19:47:13 -0800

    i added some files

--- ruben/app/assets/javascripts/init.js
+++ ruben/app/assets/javascripts/init.js
@@ -2,6 +2,13 @@ $(function() {
   var homeyApp = angular.module('homeyApp', ['ui.bootstrap', 'ngSanitize']);
   homeyApp.controller('HomeyCtrl', ['$scope', '$http', function($scope, $http) { 
     console.log('All good');
+    // In high level; what u need here is 
+    // - Call the server and get the list of all the photos; just an array of photo 
+    // filename.
+    // - Then u need to construct the photo_rows 2 dimensional array; to go over and
+    // show the photo thumbnails
+    // - Clues : use the $http service to make the ajax calls to the server.
+    // Some thing like '/home/get_photos
   }]);
 });
 
--- ruben/app/controllers/home_controller.rb
+++ ruben/app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def index
+  end
+
+  def get_photos
+    # Return the photo list array of photo file names as JSON
+  end
 end
